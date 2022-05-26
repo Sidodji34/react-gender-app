181ac8ce55713c122dec5a4e1845e93643f5de75
Sidodji34 <cj100500@gmail.com>
2022-05-26 21:10:58 +0300

    first React-App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-input">
+      <TextInput />
+      <Output />
     </div>
-  );
+  )
 }
 
-export default App;
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log('Form sent');
+}
+
+function Button(props) {
+  return (
+    <button className="submit" type="submit">{props.placeholder}</button>
+  )
+}
+
+function TextInput() {
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <input className="input" type="text" placeholder="Enter a name..." />
+      <Button placeholder='Submit'/>
+    </form>
+  )
+}
+
+function Output() {
+  return (
+    <div className="output"></div>
+  )
+}
+
+export default Main;
