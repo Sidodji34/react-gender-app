e862ccb91fa55e6bd241c713db79cce3bb97d945
Sidodji34 <cj100500@gmail.com>
2022-05-31 21:47:22 +0300

    First React-App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,101 @@
+import React from 'react';
 import './App.css';
+const serverURL = "https://api.genderize.io";
 
 export function Main() {
   return (
     <div className="body-input">
       <TextInput />
-      <Output />
     </div>
   )
 }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  console.log('Form sent');
-}
-
-function Button(props) {
+function Button() {
   return (
-    <button className="submit" type="submit">{props.placeholder}</button>
+    <button className="submit" type="submit">Submit</button>
   )
 }
 
-function TextInput() {
+class TextInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '', name: '', gender: '', valid: true };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('Send name ' + this.state.value);
+    console.log('Props ' + this.state.gender);
+    this.requestApi();
+    this.setState({ value: '' });
+  }
+  
+  requestApi() {
+    try {
+      if (this.state.value) {
+      const url = `${serverURL}?name=${this.state.value}`;
+      fetch(url).then(response => response.json())
+        .then((result) => {
+          this.setState({ name: result.name, gender: result.gender })
+          console.log(result);
+        })
+      }
+    } catch (error) {
+      console.log(`${error.name}: ${error.message}`);
+    }
+  }
+
+  render() {
+    const name = this.state.name;
+    let output;
+    if (name.length < 3 && name !== '') {
+      output = <ErrorMessage />
+    } else {
+      output = <Output name={this.state.name} gender={this.state.gender} />
+    }
+    return (
+      <form action="" onSubmit={this.handleSubmit}>
+        <input className="input" type="text" placeholder="Enter a name..."
+          onChange={this.handleChange} value={this.state.value} />
+        <Button />
+        {output}
+      </form>
+    )
+  }
+}
+
+function Output(props) {
+    return (
+      <div className="output">{props.name}  {props.gender}</div>
+    )
+}
+
+
+function ErrorMessage() {
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <input className="input" type="text" placeholder="Enter a name..." />
-      <Button placeholder='Submit'/>
-    </form>
+    <div className='error-message'>Name is too short.</div>
   )
 }
 
-function Output() {
-  return (
-    <div className="output"></div>
-  )
-}
+
+
+/*
+const value = this.state.value;
+    if (value.length < 3) {
+      this.setState({ valid: false });
+      console.log(this.state.valid);
+      return;
+    }
+    this.requestApi();
+    this.setState({ value: '' });
+  }
+  */
 
 export default Main;
